e2ffdda92b6772a4b8f65104dfeb98942923912e
Demir Altay <daltay15@gmail.com>
2023-01-30 23:08:44 -0600

    Added groupAnagrams and changed naming structure

---
+++ 0 Arrays & Hashing/3groupAnagrams.py
@@ -0,0 +1,22 @@
+List = list
+from collections import defaultdict
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Optimal m*n solution
+        res = defaultdict(list) # mapping charCount to list of Anagrams
+
+        for s in strs:
+            count = [0] * 26 # a-z
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1 # subtract ascii value of current char from ascii of a to get position in hashmap
+            
+            res[tuple(count)].append(s)
+        return res.values()
+
+if __name__ == "__main__":
+    print(Solution().groupAnagrams(strs))
+    
