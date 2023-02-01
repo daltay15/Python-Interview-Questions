e3a6e0be4f6a45affccf6590d22f1071516682f6
Demir Altay <daltay15@gmail.com>
2023-02-01 06:38:28 -0600

    Create 4sameTree.py

---
+++ 6 Trees/4sameTree.py
@@ -0,0 +1,29 @@
+# Problem: Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+
+        if not p or not q or p.val != q.val:
+            return False
+        
+        return (self.isSameTree(p.left, q.left) and 
+                self.isSameTree(p.right, q.right))
+
+
+if  __name__ == "__main__":
+    p = TreeNode(1, TreeNode(2), TreeNode(3))
+    q = TreeNode(1, TreeNode(2), TreeNode(3))
+    print(Solution().isSameTree(p, q))
