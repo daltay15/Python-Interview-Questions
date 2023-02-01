6aaa5330c20860e60611c8a43b75b8b69ed5912e
Demir Altay <daltay15@gmail.com>
2023-02-01 01:24:36 -0600

    Updated Comments

--- 6 Trees/0invertBinaryTree.py
+++ 6 Trees/0invertBinaryTree.py
@@ -6,28 +6,28 @@ from typing import Optional
 
 # Definition for a binary tree node.
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+    def __init__(self, val=0, left=None, right=None):   # Constructor
+        self.val = val  # Value
+        self.left = left   # Left child
+        self.right = right  # Right child
     
-    def __str__(self):
-        return str(self.val) + " -> " + str(self.left) + " -> " + str(self.right)
+    def __str__(self):  # For printing
+        return str(self.val) + " -> "  + str(self.left) + " -> " + str(self.right) # Inorder traversal
         
 class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root:
-            return None
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:   
+        if not root:    # If the root is None,
+            return None 
 
         # Swap the children
-        temp = root.left
-        root.left = root.right
-        root.right = temp
+        temp = root.left    # Store the left child in a temp variable
+        root.left = root.right  # Set the left child to the right child
+        root.right = temp   # Set the right child to the temp variable
 
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+        self.invertTree(root.left)  # Recursively call the function on the left child
+        self.invertTree(root.right) # Recursively call the function on the right child
 
-        return root
+        return root # Return the root
     
     
 if __name__ == "__main__":
--- 6 Trees/1maxDepthOfBinaryTree.py
+++ 6 Trees/1maxDepthOfBinaryTree.py
@@ -11,10 +11,10 @@ from collections import deque
 
 # Definition for a binary tree node.
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+    def __init__(self, val=0, left=None, right=None):   # Constructor
+        self.val = val  # Value
+        self.left = left    # Left child
+        self.right = right  # Right child
 
 root = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
 
@@ -36,38 +36,38 @@ class Solution:
         if not root:
             return 0
         
-        level = 0
-        q = deque([root])
+        level = 0   # Keep track of the level
+        q = deque([root])   # Add the root to the queue
 
-        while q:
+        while q:    # While the queue is not empty
 
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left:
+            for i in range(len(q)): # Iterate through the queue
+                node = q.popleft()  # Remove the first element from the queue
+                if node.left:   # If the node has a left child, add it to the queue
                     q.append(node.left)
-                if node.right:
+                if node.right:  # If the node has a right child, add it to the queue
                     q.append(node.right)
 
-            level += 1
-        return level
+            level += 1  # Increment the level
+        return level    # Return the level
 
     
     # Approach: Use DFS to traverse the tree and return the max depth of the tree.
     # Time Complexity: O(n)
     # Space Complexity: O(n)
     def maxDepthDFS(self, root: Optional[TreeNode]) -> int:
-        stack = [[root, 1]]
-        result = 0
+        stack = [[root, 1]] # Add the root to the stack
+        result = 0  # Keep track of the max depth
 
-        while stack:
-            node, depth = stack.pop()
+        while stack:    # While the stack is not empty
+            node, depth = stack.pop()   # Remove the last element from the stack
 
-            if node:
-                result = max(result, depth)
-                stack.append([node.left, depth + 1]) # Add left child to stack
-                stack.append([node.right, depth + 1]) # Add right child to stack
+            if node:    # If the node is not None
+                result = max(result, depth) # Update the max depth
+                stack.append([node.left, depth + 1]) # Add the left child to the stack
+                stack.append([node.right, depth + 1])   # Add the right child to the stack
 
-        return result
+        return result   # Return the max depth
 
         
         
